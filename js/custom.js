53ad320b225192c5c7874ee4d75147472b8d59bd
Stacy Harrison <stacy.e.harrison@gmail.com>
2017-03-17 18:42:24 -0400

    added error handling for misspellings, refactored to respond to enter key and autofocus

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,22 @@
 // add enter key functionality
 
-$('#wiki-submit').click(function() {
-  var wikiQuery = $('#wiki-input').val();
+$('#wiki-submit').click(wikiViewer);
+$('#wiki-input').keypress(function() {
+  if (event.which == 13) {
+    wikiViewer();
+  }
+})
 
-  // Error handling if left blank
-  if(wikiQuery === "") {
-    $('.error').html('<span class="error">Looks like you left the search blank. Please try again!</span>');
-  } else {
-    fetchJSON(wikiQuery);
-  };
-});
+function wikiViewer() {
+    var wikiQuery = $('#wiki-input').val();
+
+    // Error handling if left blank
+    if(wikiQuery === "") {
+      $('.error').html('<span class="error">Looks like you left the search blank. Please try again!</span>');
+    } else {
+      fetchJSON(wikiQuery);
+    };
+  }
 
 function fetchJSON(wikiQuery) {
   var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + wikiQuery + "&format=json&callback=?";
@@ -21,7 +28,6 @@ function fetchJSON(wikiQuery) {
     async: false,
     dataType: 'json',
     success: function(data) {
-      console.log(data[3][0]);
       placeResults(data);
     },
     error: function(error) {
@@ -37,10 +43,14 @@ function placeResults(data) {
 
   var resultsTitles = data[1];
 
-  $('.searching').html('Showing search results for <em>"' + data[0] + '"</em>');
+  if (data[1].length === 0) {
+    $('.error').html('<span class="error">I\'m sorry, there aren\'t any results. Check your spelling.</span>');
+  } else {
+    $('.searching').html('Showing search results for <em>"' + data[0] + '"</em>');
 
-  for (var i = 0; i < data[1].length; i++) {
-    $('.wiki-results').append('<a href="' + data[3][i] + '" target="_blank"><div class="result"><h2>' + data[1][i] + '</h2><p>' + data[2][i] + '</p></div></a>');
+    for (var i = 0; i < data[1].length; i++) {
+      $('.wiki-results').append('<a href="' + data[3][i] + '" target="_blank"><div class="result"><h2>' + data[1][i] + '</h2><p>' + data[2][i] + '</p></div></a>');
+    }
   }
 
 }
