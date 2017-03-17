113e1865d4dc178ee85859349ed960967901f3f3
Stacy Harrison <stacy.e.harrison@gmail.com>
2017-03-17 18:28:20 -0400

    finished results functionality, refactored js

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,17 @@
-var wikiSubmit = document.getElementById('wiki-submit');
+// add enter key functionality
 
 $('#wiki-submit').click(function() {
-  var wikiQuery = document.getElementById('wiki-input').value;
+  var wikiQuery = $('#wiki-input').val();
 
   // Error handling if left blank
   if(wikiQuery === "") {
-    console.log("Left Blank");
+    $('.error').html('<span class="error">Looks like you left the search blank. Please try again!</span>');
   } else {
     fetchJSON(wikiQuery);
   };
 });
 
 function fetchJSON(wikiQuery) {
-  // replace spaces with %20
-  wikiQuery = wikiQuery.replace(/ /g,"%20");
-
   var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + wikiQuery + "&format=json&callback=?";
   var request = new XMLHttpRequest();
 
@@ -24,7 +21,8 @@ function fetchJSON(wikiQuery) {
     async: false,
     dataType: 'json',
     success: function(data) {
-      console.log(data[2]);
+      console.log(data[3][0]);
+      placeResults(data);
     },
     error: function(error) {
       $('.error').html('<span class="error">Looks like there was an error contacting the server. Please let the webmaster know.</span>');
@@ -32,6 +30,17 @@ function fetchJSON(wikiQuery) {
   })
 }
 
-function placeResults(results) {
+function placeResults(data) {
+  $('.wiki-results').empty(); // clear previous results
+  $('.error').empty();        // clear errors
+  $('#wiki-submit').empty();  // clear search query
+
+  var resultsTitles = data[1];
+
+  $('.searching').html('Showing search results for <em>"' + data[0] + '"</em>');
+
+  for (var i = 0; i < data[1].length; i++) {
+    $('.wiki-results').append('<a href="' + data[3][i] + '" target="_blank"><div class="result"><h2>' + data[1][i] + '</h2><p>' + data[2][i] + '</p></div></a>');
+  }
 
 }
