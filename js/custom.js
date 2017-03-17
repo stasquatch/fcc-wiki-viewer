088c33d1aa016f2d80d32bd110e50cfdb575ffc3
Stacy Harrison <stacy.e.harrison@gmail.com>
2017-03-17 17:44:56 -0400

    fixed api fetch, refactored js into jquery to be more concise

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 var wikiSubmit = document.getElementById('wiki-submit');
 
-wikiSubmit.addEventListener("click", function() {
+$('#wiki-submit').click(function() {
   var wikiQuery = document.getElementById('wiki-input').value;
 
   // Error handling if left blank
@@ -12,14 +12,26 @@ wikiSubmit.addEventListener("click", function() {
 });
 
 function fetchJSON(wikiQuery) {
-  var urlBase = "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=";
-  var urlEnd = "&rvprop=content&format=json&callback=?";
-  var request = new XMLHttpRequest();
-
   // replace spaces with %20
   wikiQuery = wikiQuery.replace(/ /g,"%20");
 
-  $.getJSON(urlBase + wikiQuery + urlEnd, function(data) {
-      console.log(data.query.pages);
+  var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + wikiQuery + "&format=json&callback=?";
+  var request = new XMLHttpRequest();
+
+  $.ajax({
+    type: 'GET',
+    url: url,
+    async: false,
+    dataType: 'json',
+    success: function(data) {
+      console.log(data[2]);
+    },
+    error: function(error) {
+      $('.error').html('<span class="error">Looks like there was an error contacting the server. Please let the webmaster know.</span>');
+    }
   })
+}
+
+function placeResults(results) {
+
 }
